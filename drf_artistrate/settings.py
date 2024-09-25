29173a0b1aecaf740f4991554e3cd2cd65dd1514
Jayadeep Ravuri <jayadeepravuri@gmail.com>
2024-09-25 17:03:24 +0000

    update

--- drf_artistrate/settings.py
+++ drf_artistrate/settings.py
@@ -82,7 +82,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'cloudinary_storage', 
     'django.contrib.staticfiles',
-    'cloudinary'
+    'cloudinary',
     'rest_framework',
     'django_filters',
     'rest_framework.authtoken',
