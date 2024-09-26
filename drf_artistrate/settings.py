9acc73916a075061ad5a16c12abf22a6ed0ef899
Jayadeep Ravuri <jayadeepravuri@gmail.com>
2024-09-26 11:07:41 +0000

    updated settings.py

--- drf_artistrate/settings.py
+++ drf_artistrate/settings.py
@@ -59,7 +59,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True
+DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
     os.environ.get('ALLOWED_HOST'),
