aaedbc9bc8f651a5281803c5b3709cb7fb2e58f4
Jayadeep Ravuri <jayadeepravuri@gmail.com>
2024-10-02 11:29:22 +0000

    update settings

--- drf_artistrate/settings.py
+++ drf_artistrate/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import re
 
 if os.path.exists('env.py'):
     import env
@@ -68,7 +69,7 @@ ALLOWED_HOSTS = [
     '127.0.0.1', 
     'localhost', 
     'https://drf-artistrate-2125d79c56b7.herokuapp.com',
-    'drf-artistrate-2125d79c56b7.herokuapp.com'
+    'drf-artistrate-2125d79c56b7.herokuapp.com',
     ]
 
 
@@ -116,14 +117,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-if 'CLIENT_ORIGIN' in os.environ:
-     CORS_ALLOWED_ORIGINS = [
-         os.environ.get('CLIENT_ORIGIN')
-     ]
-else:
-     CORS_ALLOWED_ORIGIN_REGEXES = [
-         r"^https://.*\.codeinstitute-ide\.net$",
-     ]
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        rf"{extracted_url}(eu|us)\d+\w\.codeinstitute-ide\.net$",
+    ]
 
 CORS_ALLOW_CREDENTIALS = True
 
